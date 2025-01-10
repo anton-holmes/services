b2ec90b588121b66bb73b2aeeeed2b09472ac194
anton-holmes <60772965+anton-holmes@users.noreply.github.com>
2025-01-10 08:33:34 +0300

    Update jupyter_docker.sh

--- jupyter/jupyter_docker.sh
+++ jupyter/jupyter_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -itd -p 10000:8888 -v ./data:/home/jovyan/data --restart=unless-stopped jupyter/scipy-notebook
+sudo docker run -itd -p 10000:8888 -v ./data:/home/jovyan/data -e GRANT_SUDO=yes --restart=unless-stopped  quay.io/jupyter/datascience-notebook:lates
